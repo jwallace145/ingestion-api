816b797045ce0e6e66f7ebce0cd095533c2df176
James  Wallace <jameswallace@Jamess-MBP.lan1>
2019-09-19 14:24:36 -0400

    first commit

---
+++ src/main/java/com/finance/ingestion/IngestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.finance.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestionApiApplication.class, args);
+	}
+
+}
